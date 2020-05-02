fa3d32a7d741eda3eb10105304a0176b347baa0b
MinorvaFalk <minovfalk@gmail.com>
2020-05-02 22:15:23 +0700

    add sql username admin password admin

---
+++ cred.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 02, 2020 at 03:15 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sekolah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cred`
+--
+
+CREATE TABLE `cred` (
+  `roleid` int(1) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `salt` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cred`
+--
+
+INSERT INTO `cred` (`roleid`, `email`, `password`, `salt`) VALUES
+(1, 'admin@school.com', '21232f297a57a5a743894a0e4a801fc3', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cred`
+--
+ALTER TABLE `cred`
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `roleid` (`roleid`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cred`
+--
+ALTER TABLE `cred`
+  ADD CONSTRAINT `cred_ibfk_1` FOREIGN KEY (`roleid`) REFERENCES `role` (`roleid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
