b8e346f65c14f89e16014cc9b83ff32d4f85b857
edgarcnp <edgarchristian2000@gmail.com>
2020-05-14 19:28:21 +0700

    ganti sql

--- sekolah.sql
+++ sekolah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: May 14, 2020 at 07:04 AM
--- Server version: 5.7.24
--- PHP Version: 7.3.2
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2020 at 02:25 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.39
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,6 +42,33 @@ INSERT INTO `cred` (`roleid`, `email`, `password`) VALUES
 (1, 'admin@school.com', '$2y$10$GLiHdrOH6qmE16lljJCfK.ETWuQwGpRl1dPKkvUXHpWbOZiIkZCSu'),
 (3, 'student@school.com', '$2y$10$0/2QIEdTLeriPOFPLgOB8.x2I7vnrB1Ie05LiFfHEdu9oqK7CeTUa'),
 (2, 'teacher@school.com', '$2y$10$/KUDGKMjGUP6dtLHo/E5xeVBOJNPeew9RNVMoQ3iUEPHpB.drQJyG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datasiswa`
+--
+
+CREATE TABLE `datasiswa` (
+  `nis` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `namadepan` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `namablkg` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `tgllahir` date NOT NULL,
+  `telportu` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `kelas` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `nilaitugas` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nilaiuts` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nilaiuas` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nilaiakhir` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `keterangan` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `datasiswa`
+--
+
+INSERT INTO `datasiswa` (`nis`, `namadepan`, `namablkg`, `tgllahir`, `telportu`, `kelas`, `nilaitugas`, `nilaiuts`, `nilaiuas`, `nilaiakhir`, `keterangan`) VALUES
+('10001', 'Harvard', 'Senior', '2000-02-29', '085885856969', 'F', '0', '0', '0', '0', 'REMEDIAL!');
 
 -- --------------------------------------------------------
 
