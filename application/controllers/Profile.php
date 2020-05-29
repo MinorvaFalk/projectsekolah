7558c8dfaa0cba4cdaff5b351d131b8d25483d0a
haverzon <harvard.hars133@gmail.com>
2020-05-29 13:11:10 +0700

    nyoba hilangin tulisan index.php di link pake file .htaccess dari codeigniter.com>documentation

--- application/controllers/Profile.php
+++ application/controllers/Profile.php
@@ -5,7 +5,6 @@ class Profile extends CI_Controller
     public function index($nama = '')
     {
         $data['judul'] = "Profile - UAS Penilaian Siswa";
-        $data['nama'] = $nama;
         $this->load->view('include/navbar', $data);
         $this->load->view('profile/index');
         $this->load->view('include/footer');
