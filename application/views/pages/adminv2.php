77433700d6c4c82d4f038d8efed778cb49fef696
edgarcnp <edgarchristian2000@gmail.com>
2020-05-31 17:58:16 +0700

    meh

--- application/views/pages/adminv2.php
+++ application/views/pages/adminv2.php
@@ -4,12 +4,6 @@
 
 <head>
 
-		<!-- CSS styles -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-
-	<!-- JS Libs -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js" type="text/javascript"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="">
